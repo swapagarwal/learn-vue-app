cf96a3a94eedb344a15b14b244d3434b09490e96
Swapnil Agarwal <hi+github@swapnil.net>
2019-05-31 18:30:45 +0530

    add introduction to vue

---
+++ pages/introduction.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='tutorial'>
+    <h1>
+      Hello {{ name }}! Welcome to learn-vue-app.
+    </h1>
+    <br>
+    Vue is a progressive + reactive framework.
+    <br>
+    TODO: Explain progressive?
+    <br>
+    TODO: Explain reactive?
+    <br><br>
+    What's your name? <input v-model='name'>
+    <br><br>
+    Brownie Tasks:
+    <br>1. Install devtools (<a href='https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd' target='_blank'>Chrome</a> | <a href='https://addons.mozilla.org/en-US/firefox/addon/vue-js-devtools/' target='_blank'>Firefox</a>)
+    <br>2. Right click in the browser and click on inspect to open the console.
+    <br>3. Change the name to something else and see it reflected on the page.
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'foo'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
