1bf0dc37174926f1b74ebac1c2a1cd3d28e683de
Swapnil Agarwal <hi+github@swapnil.net>
2019-07-10 15:07:43 +0530

    update introduction

--- pages/introduction.vue
+++ pages/introduction.vue
@@ -3,23 +3,23 @@
     <h2>What's the buzz around Vue.js, anyway?</h2>
     Vue (pronounced like view) is a progressive framework for building user interfaces.
     <br><br>
-    <h2>Ummm, what do you mean by progressive?</h2>
+    <h2>Umm, what do you mean by progressive?</h2>
     So the idea behind calling Vue a progressive framework (and not just another javascript framework) is that you can start small and gradually build something bigger. Basically you can just drop Vue.js into your project and adopt it incrementally.
     <br><br>
-    <h2>Well, is that all?</h2>
-    Glad you asked. Vue is also reactive - so when our data changes, Vue takes care of updating it at all the places it's being used.
+    <h2>Well, is that all..?</h2>
+    Glad you asked! Vue is also reactive - so when any data changes, Vue takes care of updating it at all the places it's being used.
     <br><br>
-    But wait... there's more! Vue is:
+    But wait... there's more! Vue is also:
     <ol>
-      <li>Approachable - If you know HTML, CSS, and JavaScript, you can get onboarded in no time.</li>
-      <li>Versatile - Vue can be used as a library as well as a framework.</li>
-      <li>Performant - It's blazing fast and adds only 20KB to your bundle size.</li>
-      <li>Maintainable - As you'll see in the "Single File Component" tutorial, Vue makes it effortless to manage complex interfaces as well.</li>
-      <li>Testable - This is a prerequisite for any new framework. 🤷‍♂️</li>
+      <li>🤗 Approachable - If you know HTML, CSS, and JavaScript, you can get onboarded in no time.</li>
+      <li>💪 Versatile - Vue can be used as a library as well as a framework.</li>
+      <li>⚡ Performant - It's blazing fast and adds only 20KB to your bundle size.</li>
+      <li>✍️ Maintainable - As you'll see in the "Single File Component" tutorial, Vue makes it effortless to manage complex interfaces as well.</li>
+      <li>🧪 Testable - This is more like a prerequisite for any new framework nowadays. 🤷‍♂️</li>
     </ol>
     <br>
-    <h2>I'm sold, what now?</h2>
-    Head over to the <nuxt-link to="/hello-world">next tutorial</nuxt-link> and enjoy the view!
+    <h2>I'm sold! What now?</h2>
+    Head over to the <nuxt-link to="/hello-world">next tutorial</nuxt-link> and enjoy the vue! (see what I did there? 😏)
     <br><br>
     <h2>Fun Facts</h2>
     <ul>
