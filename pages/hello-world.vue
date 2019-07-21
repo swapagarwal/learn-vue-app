eb4d3857596af129d4aad293b4256274d06b1f4d
Swapnil Agarwal <hi+github@swapnil.net>
2019-07-21 19:17:03 +0530

    update hello world

--- pages/hello-world.vue
+++ pages/hello-world.vue
@@ -1,7 +1,34 @@
 <template>
   <div class='tutorial'>
+    Let's start! 👊
+    <br><br>
+    Here's a sample HTML file:
     <pre v-highlightjs="html"><code class="html"></code></pre>
+    <br>
+    Now, I had promised you in the introduction that you can start small with Vue. So to add Vue here, you can just load Vue via CDN like this:
+    <pre v-highlightjs="unpkg"><code class="html"></code></pre>
+    <br>
+    Next, we modify the "div" to include an "id" (we'll see why below) and use the "name" via template syntax (double curly braces):
+    <pre v-highlightjs="el"><code class="html"></code></pre>
+    <br>
+    Finally, we create a Vue instance using:
+    <pre v-highlightjs="app"><code class="html"></code></pre>
+    <br>
+    Two things to note here:
+    <ol>
+      <li>"el" takes the CSS selector of the element where we want to attach the Vue instance (that's why we added it before!).</li>
+      <li>"data" contains the model object which will be accessible inside the element selected above.</li>
+    </ol>
+    <br>
+    And we are done! Try this hands-on in the below exercise. I recommend you to complete this before moving on to the next tutorial. Open the file at the location mentioned on the right side and make the appropriate changes.
+    <br><br>
     <Exercise/>
+    Refresh the page to see the changes reflected above.
+    <br><br>
+    Done? Does your output (right side) match the expected output (left side)?
+    <br>
+    If yes, congratulations! You've successfully started your Vuetiful journey! See you in the next tutorial.
+    <br>
     <h2>Hello {{ name }}! Welcome to learn-vue-app.</h2>
     <br>
     What's your name? <input v-model='name'>
@@ -47,13 +74,25 @@
       return {
         name: 'foo',
         html: `<html>
-    <head></head>
+    <head>...</head>
     <body>
         <div>
             <h1>Hello John Doe! Welcome to learn-vue-app.</h1>
         </div>
     </body>
-</html>`
+</html>`,
+        unpkg: `<script src="https://unpkg.com/vue"><\/script>`,
+        app: `<script>
+    const app = new Vue({
+        el: '#baby-steps',
+        data: {
+            name: 'Foo Bar'
+        }
+    })
+<\/script>`,
+        el: `<div id="baby-steps">
+    <h1>Hello {{ name }}! Welcome to learn-vue-app.</h1>
+</div>`
       }
     }
   }
