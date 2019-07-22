e0dda82c1cc8970ce0a130158a4f626ed6744b5b
Swapnil Agarwal <hi+github@swapnil.net>
2019-07-22 19:24:44 +0530

    move browser setup to its own tutorial

--- pages/hello-world.vue
+++ pages/hello-world.vue
@@ -28,36 +28,6 @@
     Done? Does your output (right side) match the expected output (left side)?
     <br>
     If yes, congratulations! You've successfully started your Vuetiful journey! See you in the next tutorial.
-    <br>
-    <h2>Hello {{ name }}! Welcome to learn-vue-app.</h2>
-    <br>
-    What's your name? <input v-model='name'>
-    <br><br>
-    This page (or rather the whole website) is rendered using Vue. Try changing the value in the above input and you'll see it reflected in the greeting message.
-    <br><br>
-    <h2>Brownie Tasks</h2>
-    <ol>
-      <li>
-        Install devtools (<a href='https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd' target='_blank'>Chrome</a> | <a href='https://addons.mozilla.org/en-US/firefox/addon/vue-js-devtools/' target='_blank'>Firefox</a>)
-      </li>
-      <li>
-        Right click in the browser, click on "Inspect Vue component" and open the Vue tab.
-      </li>
-      <li>
-        Make sure the "PagesHelloWorld" line is selected.
-      </li>
-      <li>
-        Edit the variable "name" in the section on your right hand side.
-        <br>
-        OR
-        <br>
-        Switch to the console and change the name to something else (using $vm0.name = "bar";).
-        <br>
-      </li>
-      <li>
-        You'll see the updated greeting message on the page.
-      </li>
-    </ol>
     <TutorialNavigation/>
   </div>
 </template>
@@ -72,7 +42,6 @@
     },
     data() {
       return {
-        name: 'foo',
         html: `<html>
     <head>...</head>
     <body>
