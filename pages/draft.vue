66e60858fef164b8e5bbfc711ddd88419d4691eb
Swapnil Agarwal <hi+github@swapnil.net>
2019-07-26 19:36:17 +0530

    add draft

---
+++ pages/draft.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <pre>
+      This is a place for me to jot down unsorted ideas. This component should be removed once the tutorial is complete!
+
+      Lists + Events
+      Conditionals + Loops
+
+      (recap - boots are in stock)
+
+      Components (HTML Tags)
+      Anatomy of a .vue file (SFC)
+      Extensible - Pug, TypeScript, SASS
+      Slots
+      Nested Components, Communication using Events
+
+      Computed Properties
+      Watchers
+
+      Tables
+      Forms
+
+      vote zero config buttons
+      Routing
+      State Management
+      SSR
+
+      Mixins
+      Lifecycle
+
+      Plugins
+      REST API Calls
+      Animations
+      NativeScript
+    </pre>
+    <TutorialNavigation/>
+  </div>
+</template>
+
+<script>
+  import TutorialNavigation from '~/components/TutorialNavigation.vue'
+  export default {
+    components: {
+      TutorialNavigation
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
