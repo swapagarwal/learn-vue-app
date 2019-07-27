49b9fc0e2222ec0b320f6f01ce238c804c1a710c
Swapnil Agarwal <hi+github@swapnil.net>
2019-07-27 10:52:19 +0530

    update data binding

--- pages/data-binding.vue
+++ pages/data-binding.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-    <h1 :class="className">Hello World</h1>
-    <button :title="title">Hover Over Me</button>
-    <button :class=" { 'is-loading': isLoading } " @click="toggleIsLoading">Click Me</button>
-
-    data-binding
-    v-bind:attribute="expression"
-    eg: v-bind:class="['foo', 'bar']"
-    :
-
-    v-on:event.modifier="expression"
-    eg: v-bind:keypress.shift.ctrl.a="selectAll"
-    @
-
-    v-bind:value="value"
-    v-on:change="event => value = event.target.value"
-    ===
-    v-model="value"
+  <div class="tutorial">
+    In simple terms, data binding refers to the ability of views to automatically update when models (data) change.
+    <br><br>
+    You can use the "v-bind" attribute to attach data to an element. The syntax is:
+    <pre v-highlightjs="syntax"><code class="html"></code></pre>
+    <br>
+    For example, we can add a class to an element like this:
+    <pre v-highlightjs="example"><code class="html"></code></pre>
+    <br>
+    Let's assign a value to the "className" variable and define the style for "color-red" class.
+    <pre v-highlightjs="assign"><code class="html"></code></pre>
+    <br>
+    And we get the heading text, styled in red color, as follows:
+    <h1 v-bind:class="className">Hello World</h1>
+    <br>
+    Try out the exercise below.
     <TutorialNavigation/>
   </div>
 </template>
@@ -30,13 +28,22 @@
     data() {
       return {
         className: 'color-red',
-        title: 'Now the title is being set via JavaScript.',
-        isLoading: false
-      }
-    },
-    methods: {
-      toggleIsLoading() {
-        this.isLoading = ! this.isLoading;
+        syntax: `v-bind:attribute="expression"`,
+        example: `<h1 v-bind:class="className">Hello World</h1>`,
+        assign: `<script>
+    export default {
+        data() {
+            return {
+                className: 'color-red'
+            }
+        }
+    }
+<\/script>
+<style scoped>
+.color-red {
+    color: red;
+}
+<\/style>`
       }
     }
   }
@@ -45,9 +52,6 @@
 <style scoped>
 .color-red {
   color: red;
-}
-.is-loading {
-  background: red;
 }
 .tutorial {
   width: 80%;
